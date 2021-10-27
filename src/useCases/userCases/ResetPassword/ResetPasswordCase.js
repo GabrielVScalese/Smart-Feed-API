2b30acd679b1de255d6a498c2a511a65310cabc4
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-27 10:33:06 -0300

    Mudanca do email de reset de senha

--- src/useCases/userCases/ResetPassword/ResetPasswordCase.js
+++ src/useCases/userCases/ResetPassword/ResetPasswordCase.js
@@ -45,7 +45,7 @@ class ResetPasswordCase {
         email: process.env.EMAIL,
       },
       subject: "Recuperar senha da conta",
-      body: `<p>Olá ${user["name"]}, acesse esse link para recuperar sua senha: <a>https://smart-feed-web.vercel.app/users/editPassword/${resetToken["id"]}</a></p>`,
+      body: `<p>Olá ${user["name"]}, acesse esse link para recuperar sua senha: <a>https://smart-feed-web.vercel.app/users/resetPassword/${resetToken["id"]}</a></p>`,
     });
 
     return resetToken["id"];
